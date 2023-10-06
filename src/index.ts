48e5dd3353d2109b98b203d7542c036297778fb1
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-10-06 09:41:02 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -19,14 +19,9 @@ dotenv.config()
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
-
 app.use(cors({ origin: [`http://localhost:${PORT}`, `http://localhost:3000`], }));
 
-
 app.listen(PORT, () => console.log(`> BetPoeli deployed on http://localhost:${PORT}`));
-
 
 app
 .use(bodyParser.json())
