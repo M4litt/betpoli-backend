6413c4db369e1b1d0931caab6b8e278e570eb4e4
Kaligrámetro <81710127+CalifanoFrancisco@users.noreply.github.com>
2023-10-20 10:55:03 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,683 @@
-//* Deps
-import express from "express"
-import bodyParser from 'body-parser'
-import mongoose from "mongoose"
-import dotenv from 'dotenv'
-import cors from 'cors'
-import swaggerUi from 'swagger-ui-express'
-import * as swaggerDocument from "./swagger.json"
-import fs from "fs"
+// * Deps
+import express, { Request, Response } from 'express';
+import * as swaggerDocument           from './swagger.json'
+import fileUpload                     from 'express-fileupload';
+import swaggerUi                      from 'swagger-ui-express'
+import bodyParser                     from 'body-parser';
+import mongoose                       from "mongoose";
+import dotenv                         from 'dotenv'
+import cors                           from 'cors'
+// * Routers
+import { PeriodistaRouter }           from './routes/periodista.routes';
+import { partidoRouter }              from './routes/partido.routes';
+import { rutasApuestas }              from './routes/apuesta.routes';
+import { equipoRouter }               from './routes/equipo.routes';
+import { AdminRouter }                from './routes/admin.routes';
+import { rutasUsuarios }              from './routes/user.routes';
+import { ligaRouter }                 from './routes/liga.routes';
+import { paisRouter }                 from './routes/pais.routes';
+import { uploadFile } from './controllers/upload.controller';
 
-//* Routers
-import { rutasUsuarios }    from "./routes/user.routes"
-import { PeriodistaRouter } from "./routes/periodista.routes"
-import { equipoRouter }     from "./routes/equipo.routes"
-import { AdminRouter }      from "./routes/admin.routes"
-import { partidoRouter }    from "./routes/partido.routes"
-import { rutasApuestas }    from './routes/apuesta.routes'
-import { ligaRouter }       from "./routes/liga.routes"
-import { paisRouter }       from "./routes/pais.routes"
+dotenv.config();
 
-dotenv.config()
+const app  = express();
+const PORT = process.env.PORT || 3000;
 
-const app = express()
-const PORT = process.env.PORT || 3000
-
+// hay conflictos entre los swaggers
+/*
+import fs                             from "fs"
 const swaggerFile = (process.cwd()+"/swagger.json")
 const swaggerData = fs.readFileSync(swaggerFile, 'utf8')
 const swaggerDocument = JSON.parse(swaggerData)
+app
+.use('/partidos/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, undefined, undefined, undefined))
+*/
 
 app
-  .use(express.json())
-  .use(cors({ origin: [`http://localhost:${PORT}`, `http://localhost:3000`], }))
-  .get('', (req, res) => res.send("Bienvenido a mi api"))
-  .use('/api-docs',    swaggerUi.serve, swaggerUi.setup(swaggerDocument))
-  .use("/apuestas",    rutasApuestas)
-  .use("/usuarios",    rutasUsuarios)
-  .use('/usuarios',    rutasUsuarios)
-  .use('/periodistas', PeriodistaRouter)
-  .use('/equipos',     equipoRouter)
-  .use('/admin',       AdminRouter)
-  .use('/partidos',    partidoRouter)
-  .use('/partidos/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, undefined, undefined, undefined))
-  .use('/liga',        ligaRouter)
-  .use('/pais',        paisRouter)
-  .listen(PORT, () => console.log(`> BetPoeli deployed on http://localhost:${PORT}`))
+.use(cors({ origin: [`http://localhost:${PORT}`, `http://localhost:3000`], }))
+.use(bodyParser.json())
+// routes
+.get('/', (req:Request, res:Response) => res.send("Bienvenido a mi api"))
+.use('/api-docs',    swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+.use("/apuestas",    rutasApuestas)
+.use("/usuarios",    rutasUsuarios)
+.use('/usuarios',    rutasUsuarios)
+.use('/periodistas', PeriodistaRouter)
+.use('/equipos',     equipoRouter)
+.use('/admin',       AdminRouter)
+.use('/partidos',    partidoRouter)
+.use('/liga',        ligaRouter)
+.use('/pais',        paisRouter)
+// file upload
+.use(fileUpload({ limits: { fileSize: 10000000 }, abortOnLimit: true }))
+.use('/public', express.static('.public'))  // fetch files using http://<HOST>:<PORT>/public/<FILE_NAME>
+.use('/upload/:foldername', uploadFile)
+// init
+.listen(PORT, () => console.log(`> BetPoeli deployed on http://localhost:${PORT}`));
 
 mongoose
-  .set("strictQuery",  false)
-  .connect(process.env.MONGO_CON_STRING!)
-  .then(() => console.log(`> mongoDB connection initialized.`))
+.set('strictQuery',  false)
+.connect(process.env.MONGO_CON_STRING!)
+.then(() => console.log(`> mongoDB connection initialized.`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//the sexr was here
